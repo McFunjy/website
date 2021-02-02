8ddda3ed142f3e4be433f36392ff3c7cf2950029
McFunjy <barminov_01@mail.ru>
2021-02-02 04:57:39 +0300

    Initial commit

---
+++ Week4/posts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    return render(request, 'index.html', {'posts':posts})
+
+
+def  group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
